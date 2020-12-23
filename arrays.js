b1cd26739fda9af870c8aed14e60f82f509b97a6
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-12-22 18:54:46 -0800

    Update

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 const string = "12345678";
 
-// slice(-1) gives us the last char of a array -> new array
+// slice(-1) gives us the last char of a array in a new array
 console.log(array.slice(-1));
 
-// slice(-1) gives us the last char of a string -> new string
+// slice(-1) gives us the last char of a string in a new string
 console.log(string.slice(-1));
