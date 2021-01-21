a66d2d6f0fa99f991445315068330d071c961ecf
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-20 21:38:25 -0800

    Change  replace to use splice

--- arrays.js
+++ arrays.js
@@ -3,8 +3,20 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 const string = "12345678";
 
+console.log(array);
+
 // slice(-1) gives us the last char of a array in a new array
-console.log(array.slice(-1));
+// console.log(array.slice(-1));
 
 // slice(-1) gives us the last char of a string in a new string
-console.log(string.slice(-1));
+// console.log(string.slice(-1));
+
+// replace the last item in the array
+const removed = array.splice(-1, 1, 9);
+console.log(removed);  // ==>  [8] :  splice returns the deleted elements in an array
+
+// original array has beem changed
+console.log(array);
+
+console.log(array.pop());
+console.log(array);
